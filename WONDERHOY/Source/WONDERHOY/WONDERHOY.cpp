06bbdf86706faf256c4675f042014efb9ff98f20
Wooniety <wooniety@gmail.com>
2025-06-03 23:06:04 +0800

    Blank Unreal Project
    TODO mess with Unreal configs

---
+++ WONDERHOY/Source/WONDERHOY/WONDERHOY.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WONDERHOY.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WONDERHOY, "WONDERHOY" );
