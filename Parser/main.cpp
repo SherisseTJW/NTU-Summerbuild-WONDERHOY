b341babc9c83673f9dc6e259791abe64d3bc8782
Blahblahlolhahaha <pikachuuu1436@gmail.com>
2025-06-24 16:48:53 +0800

    "unreal does not like special characters apparently"
    -senpai

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -4,21 +4,22 @@ using namespace std;
 int main(){
     beatmap::Beatmap beatmap =  beatmap::parseBeatmap("test.osu");
     std::vector<beatmap::HitObject*> objects = beatmap.getHitObjects();
-    int kek[5] = {0,0,0,0,0};
-    for(beatmap::HitObject* object : objects){
-        std::cout << object->getTime() << std::endl;
-        if(object->getType() == beatmap::HitObject::SLIDER){
-            for( beatmap::Coord sad : (object)->getAnchorPoints()){
-                std::cout << sad.getX() << ":" << sad.getY() << std::endl;
-            }
-        }
-        int time = object->getTime() + (std::rand() % 252 - 126);
-        kek[beatmap.getJudgement(time, object)]++;
+    std::cout << beatmap.getAudioFileName() << std::endl;
+    // int kek[5] = {0,0,0,0,0};
+    // for(beatmap::HitObject* object : objects){
+    //     std::cout << object->getTime() << std::endl;
+    //     if(object->getType() == beatmap::HitObject::SLIDER){
+    //         for( beatmap::Coord sad : (object)->getAnchorPoints()){
+    //             std::cout << sad.getX() << ":" << sad.getY() << std::endl;
+    //         }
+    //     }
+    //     int time = object->getTime() + (std::rand() % 252 - 126);
+    //     kek[beatmap.getJudgement(time, object)]++;
         
-    }
-    int* sad = beatmap.getAllJudgements();
-    printf("%d\n", objects.size());
-    for(int i = 0; i < 5; i++){
-        printf("%d, %d\n", kek[i], sad[i]);
-    }
+    // }
+    // int* sad = beatmap.getAllJudgements();
+    // printf("%d\n", objects.size());
+    // for(int i = 0; i < 5; i++){
+    //     printf("%d, %d\n", kek[i], sad[i]);
+    // }
 }
